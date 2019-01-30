9a88a394fa5874d97d0f75e657fc13984da0a1c2
xieruishen <xierui.shen@students.olin.edu>
2019-01-29 21:27:07 -0500

    finish sofsys

--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
-void add(int x, int y);
-int main() {
-    printf("Hello, World!\n");
-    add(2,4);
-    int a = 3;
-    int b = 4;
-    int c = a + b;
-    printf("c is %d\n", c);
-    return 0;
-}
 
-void add(int x, int y){
-  int z = x + y;
-  printf("The answer is %d\n",z);
-}
+int main() {
+    int x = 5; /* The added assembly are */
+              /* subq	$16, %rsp */
+              /* movl	$5, -4(%rbp) */
+    int y = x + 1;
+    printf("Hello, World! y = %d\n", y);
+    return 0;
+  }
+
+/*
+Question 2: When turning on optimization using flag -O2,
+  additional assembly are created at the top and the body of main is shortened.
+  The line of x = 5 in assembly is removed.
+
+Question 3: The line movl	$5, -4(%rbp) becomes movl	$5, %edx which is shorter.
+  And four lines of the ass
+  The part of the assembly that is about the main function is shorter with
+  optimization and additional lines such as
+  ".section	.text.unlikely,"ax",@progbits
+.LCOLDB1:
+	.section	.text.startup,"ax",@progbits
+.LHOTB1:
+	.p2align 4,,15
+	.globl	main" are added. printf in the assembly becomes __printf_chk.
+
+Question 4
+  With optimization, instead of sotring the value of both x and y, the value of
+  y is computed during compilation and value of x is not stored in assembly.
+  Optimization increases compilation time and the performance of the generated
+  code and therefore decreases runtime by trying to reduce code size for the
+  functions.
+*/
--- exercises/ex01/hello.s
+++ exercises/ex01/hello.s
@@ -1,76 +1,34 @@
 	.file	"hello.c"
-	.section	.rodata
+	.section	.rodata.str1.1,"aMS",@progbits,1
 .LC0:
-	.string	"Hello, World!"
-.LC1:
-	.string	"c is %d\n"
-	.text
+	.string	"Hello, World! y = %d\n"
+	.section	.text.unlikely,"ax",@progbits
+.LCOLDB1:
+	.section	.text.startup,"ax",@progbits
+.LHOTB1:
+	.p2align 4,,15
 	.globl	main
 	.type	main, @function
 main:
-.LFB0:
+.LFB23:
 	.cfi_startproc
-	pushq	%rbp
+	subq	$8, %rsp
 	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	subq	$16, %rsp
-	movl	$.LC0, %edi
-	call	puts
-	movl	$4, %esi
-	movl	$3, %edi
-	call	add
-	movl	$3, -12(%rbp)
-	movl	$4, -8(%rbp)
-	movl	-12(%rbp), %edx
-	movl	-8(%rbp), %eax
-	addl	%edx, %eax
-	movl	%eax, -4(%rbp)
-	movl	-4(%rbp), %eax
-	movl	%eax, %esi
-	movl	$.LC1, %edi
-	movl	$0, %eax
-	call	printf
-	movl	$0, %eax
-	leave
-	.cfi_def_cfa 7, 8
+	movl	$6, %edx
+	movl	$.LC0, %esi
+	movl	$1, %edi
+	xorl	%eax, %eax
+	call	__printf_chk
+	xorl	%eax, %eax
+	addq	$8, %rsp
+	.cfi_def_cfa_offset 8
 	ret
 	.cfi_endproc
-.LFE0:
+.LFE23:
 	.size	main, .-main
-	.section	.rodata
-.LC2:
-	.string	"The answer is %d\n"
-	.text
-	.globl	add
-	.type	add, @function
-add:
-.LFB1:
-	.cfi_startproc
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	subq	$32, %rsp
-	movl	%edi, -20(%rbp)
-	movl	%esi, -24(%rbp)
-	movl	-20(%rbp), %edx
-	movl	-24(%rbp), %eax
-	addl	%edx, %eax
-	movl	%eax, -4(%rbp)
-	movl	-4(%rbp), %eax
-	movl	%eax, %esi
-	movl	$.LC2, %edi
-	movl	$0, %eax
-	call	printf
-	nop
-	leave
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-.LFE1:
-	.size	add, .-add
+	.section	.text.unlikely
+.LCOLDE1:
+	.section	.text.startup
+.LHOTE1:
 	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.11) 5.4.0 20160609"
 	.section	.note.GNU-stack,"",@progbits
