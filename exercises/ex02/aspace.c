914c53836cc650e05a611bb62f1b472dded1127c
xieruishen <xierui.shen@students.olin.edu>
2019-02-05 20:35:25 -0500

    finsh parts before adder

--- exercises/ex02/aspace.c
+++ exercises/ex02/aspace.c
@@ -9,18 +9,39 @@ License: GNU GPLv3
 #include <stdlib.h>
 
 int var1;
+void check_stack();
 
 int main ()
 {
     int var2 = 5;
     void *p = malloc(128);
+    void *j = malloc(128);
+    /*Heap grows up towards larger addresses. Address of j pointed to
+     is high than the address p pointed to in heap*/
     char *s = "Hello, World";
 
     printf ("Address of main is %p\n", main);
     printf ("Address of var1 is %p\n", &var1);
     printf ("Address of var2 is %p\n", &var2);
+    check_stack();
+
     printf ("p points to %p\n", p);
+    printf ("j points to %p\n", j);
     printf ("s points to %p\n", s);
 
+    /*stack grows downwards toward lower addresses. Address of local variable
+    a is lower than the address of var2.*/
+
+    void *m = malloc(10);
+    void *n = malloc(10);
+    printf ("m points to %p\n", m);
+    printf ("n points to %p\n", n);
+    /* The space between them is 0x20 which is 20 byte*/
+
     return 0;
+}
+
+void check_stack(){
+  int a = 4;
+  printf("Address of local var a is %p\n", &a);
 }
--- exercises/ex02/stack.c
+++ exercises/ex02/stack.c
@@ -10,16 +10,14 @@ License: GNU GPLv3
 
 #define SIZE 5
 
-int *foo() {
+void foo(int *array) { /*Instead of returning pointers to stack allocated data
+                          pass the pointer to the function as a parameter*/
     int i;
-    int array[SIZE];
-
     printf("%p\n", array);
 
     for (i=0; i<SIZE; i++) {
         array[i] = 42;
     }
-    return array;
 }
 
 void bar() {
@@ -36,7 +34,9 @@ void bar() {
 int main()
 {
     int i;
-    int *array = foo();
+    int array[SIZE]; /*Initialize array before foo and pass it as
+                     a pointer input parameter */
+    foo(array);
     bar();
 
     for (i=0; i<SIZE; i++) {
@@ -45,3 +45,24 @@ int main()
 
     return 0;
 }
+
+/*
+Question 1: it will print the address of the local variable array in both foo()
+and bar() and print 42 5 times if the program worked as expected.
+
+Question 2: The warning I got is function returns address of local variable.
+This means that that in foo(), it is trying to return a pointer to a local
+array that will be taken off the stack after the function ends.
+
+Question 3: A segmentation fault appears.This is because array in the main
+function is pointing to the address of a stack allocated data. This data
+disappears after the stack for the function call is removed. The print
+statements in foo and bar show that the local variable array for each has the
+same address. Therefore, The address array in the main function is illegally
+editing that same memory address and therefore yields segmentation fault.
+
+QUestion 4: WHen the print statements are commented out, I got additional
+warning from the compiler saying the local variable array in bar function is
+set but not used.
+
+*/
