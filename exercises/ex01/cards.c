cd208961e78381b83637d5a65f0bc3307e792817
gdouglass1 <59745338+gdouglass1@users.noreply.github.com>
2020-01-30 11:40:30 -0500

    Update cards.c

--- exercises/ex01/cards.c
+++ exercises/ex01/cards.c
@@ -1,6 +1,13 @@
+/* Gia Douglass
+Program that counts the value of cards entered
+*/
 #include <stdio.h>
 #include <stdlib.h>
-
+/* Prompts the user for input of a card and puts the reply in the given buffer.
+       User input is truncated to the first two characters, char size 3 for buffer. 
+       Returns card_name.
+       card_name: buffer where result is stored
+*/
 char * getCard(){
   char *card_name = (char *) malloc(sizeof(char) * 3);
   puts("Enter the card_name: ");
@@ -8,7 +15,13 @@ char * getCard(){
   return card_name;
 }
 
-int checkLetters(char * cn){
+/* Evaluates card that user input (card_name) to determine the value. 
+       Returns int value of card if a valid card (1-10, J, Q, K, A).
+       Prints "I don't understand value" if input isn't valid card.
+       If user inputs 'X', program will stop.
+       cn: card that user input
+*/
+int check(char * cn){
   int val = 0;
   switch(cn[0]) {
     case 'K':
@@ -31,14 +44,17 @@ int checkLetters(char * cn){
   }
   return val;
 }
-
+/* Main function will call on getCard() to ask the user to input card then call on check() to evaluate card value. Will output current count of all the cards the user has entered. 
+       Program will run as long as user does not input 'X'.
+       Calls on functions getCard() and check().
+*/
 int main(){
   int count = 0;
   char * cardName= getCard();
 
   while (cardName[0] != 'X'){
     int v = 0;
-    v= checkLetters(cardName);
+    v= check(cardName);
     count+= v;
     printf("Current count: %i\n", count);
     cardName= getCard();
