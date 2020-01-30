7b0e62f62a2bec1bc7c749b2bc5103fc2f1c0c72
gdouglass1 <59745338+gdouglass1@users.noreply.github.com>
2020-01-30 11:24:26 -0500

    Update cards.c

--- exercises/ex01/cards.c
+++ exercises/ex01/cards.c
@@ -1,54 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-char getCard(){
-  char card_name[3];
+
+char * getCard(){
+  char *card_name = (char *) malloc(sizeof(char) * 3);
   puts("Enter the card_name: ");
   scanf("%2s", card_name);
-  return card_name[0];
+  return card_name;
 }
-int checkLetters(char cn){
-  int i= atoi(&cn);
-  int v= 0;
-  switch(cn) {
-      case 'K':
-      case 'Q':
-      case 'J':
-        v = 10;
-        return v; 
+
+int checkLetters(char * cn){
+  int val = 0;
+  switch(cn[0]) {
+    case 'K':
+    case 'Q':
+    case 'J':
+      val = 10;
+      break;
+    case 'A':
+      val = 11;
+      break;
+    case 'X':
+      break;
+    default:
+      val = atoi(cn);
+      if((val<1) || (val >10)){
+        puts("I don't understand that value!");
+        val=0;
         break;
-      case 'A':
-        v = 11;
-        return v; 
-        break;
-      case 'X':
-        break;
+      }
   }
-  if( (i<1) || (i>10)){
-    puts("I don't understand that value!");
-    v= 0;
-    return v;
-  }
-  return v;
+  return val;
 }
-int checkNumbers(char cn){
-  int c=0; 
-  if ((cn> 2) && (cn <11)){
-    c++;
-    return c;
-  }
-  return c; 
-}
+
 int main(){
   int count = 0;
-  char cardName= getCard();
+  char * cardName= getCard();
 
-  while (cardName != 'X'){
-    int val = 0;
-    val= checkLetters(cardName);
-    count+= val;
-    if (val==0){
-      count += checkNumbers(atoi(&cardName));
-    }
+  while (cardName[0] != 'X'){
+    int v = 0;
+    v= checkLetters(cardName);
+    count+= v;
     printf("Current count: %i\n", count);
     cardName= getCard();
 
