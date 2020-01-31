1389a17bc42cc5324acbe8dfb6421c2bbddee3f3
gdouglass1 <59745338+gdouglass1@users.noreply.github.com>
2020-01-31 14:06:34 -0500

    Update hello.c

--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+void checkOddEven(int a, int b){
+  int c = a + b;
+    printf("c is %d\n", c);
+    /* if (c%2 == 0) {
+        printf("c is even\n");
+    } 
+    else {
+        printf("c is odd\n");
+    } */
+    int z= c%2;
+    switch(z){
+      case 0:
+        printf("c is even\n");
+        break;
+      default:
+        printf("c is odd\n");
+        break;
+    }
+}
 
 int main() {
     printf("Hello, World!\n");
+    int a = 3;
+    int b = 4;
+
+    checkOddEven(a, b);
     return 0;
 }
