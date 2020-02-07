d79469633d3c98638961d4918fb943aaa6644a9a
gdouglass1 <59745338+gdouglass1@users.noreply.github.com>
2020-02-07 08:24:42 -0500

    Update adder.c

--- exercises/ex02/adder.c
+++ exercises/ex02/adder.c
@@ -1,8 +1,17 @@
+/* Gia Douglass
+Program that counts the value of a max of 9 numbers entered
+*/
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
 
+/* Prompts the user for input of a number. If the number is a letter the program
+will ask again until it gets a number. If the user enters 'X', the function will end. 
+       Returns number entered.
+       str: user input
+       number: user input as number
+*/
 int getInput(){
   char str[3];
   int number;
@@ -22,6 +31,11 @@ int getInput(){
   return number;
 }
 
+/* Adds up all the numbers in integers array
+       Returns sum of numbers in integers array.
+       sum: sum of integers in array
+       c: counter of how many numbers are in the array
+*/
 int addArray(int *integers, int c){
   int sum = 0;
   for (int i = 0; i < (c); i++){
@@ -30,6 +44,11 @@ int addArray(int *integers, int c){
   return sum;
 }
 
+/* Main function will call on getInput() to ask the user to input a number then call on addArray() 
+to count up values in integers array. 
+       Program will run as long as user does not input 'X'.
+       Calls on functions getInput() and addArray().
+*/
 int main(){
    printf("You can only enter 9 numbers and each number must be less than three digits and positive (1-99).\n");
   int total=0;
